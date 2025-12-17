8522b08657220edce4a93677627dc11ae73f54f4
xiaokang <xiaokang.xiong@pixocial.com>
2025-12-17 14:38:48 +0800

    去掉Sign up入口

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,15 +39,6 @@ function Navbar() {
               </li>
             </>
           )}
-          {!isAuth && (
-            <>
-              <li className="nav-item">
-                <NavLink activeClassName="active" className="nav-link" to="/register">
-                  Sign up
-                </NavLink>
-              </li>
-            </>
-          )}
         </ul>
       </div>
     </nav>
